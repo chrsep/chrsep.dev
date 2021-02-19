2c46ad6d9f46aa404b3e0203bc4225b9187b9eba
Chrisando Pramudhita <chrsep@protonmail.com>
2021-02-19 19:46:16 +0700

    add mock blog.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,33 @@
+<script context="module">
+  export function preload() {
+    return this.fetch(`blog.json`)
+      .then((r) => r.json())
+      .then((posts) => {
+        return { posts }
+      })
+  }
+</script>
+
+<script>
+  export let posts
+</script>
+
 <svelte:head>
   <title>Sapper project template</title>
 </svelte:head>
 
-<article class="prose dark:prose-light m-3">
-  <h1>Great success!</h1>
-</article>
+<ul class="m-3">
+  {#each posts as post}
+    <!-- we're using the non-standard `rel=prefetch` attribute to
+        tell Sapper to load the data for the page as soon as
+        the user hovers over the link or taps it, instead of
+        waiting for the 'click' event -->
+    <li class="text-md hover:underline">
+      <a rel="prefetch" href="blog/{post.slug}" class="prose">
+        <h1>
+          {post.title}
+        </h1>
+      </a>
+    </li>
+  {/each}
+</ul>
