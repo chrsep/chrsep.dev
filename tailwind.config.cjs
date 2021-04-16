3cb08ce0388866ea175944d35b79e64fa8f9b325
Chrisando Pramudhita <chrsep@protonmail.com>
2021-04-16 22:25:39 +0700

    migrate to svelte kit.

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+	mode: "jit",
+	purge: [
+		"./src/**/*.{html,js,svelte,ts}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require("@tailwindcss/forms"),
+		require("@tailwindcss/typography"),
+		require("@tailwindcss/aspect-ratio"),
+	],
+};
