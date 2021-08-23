ebbcd6420d6d64dcfea238509799e11799823661
Chrisando Pramudhita <chrsep@protonmail.com>
2021-08-23 15:23:18 +0700

    add brand colors.

--- src/lib/TechPills.svelte
+++ src/lib/TechPills.svelte
@@ -4,13 +4,15 @@
   export let text: string;
   export let web: string;
   export let icon: string;
+  export let ringHoverClass: string;
+  export let iconGroupHoverClass: string;
 </script>
 
 <li class="flex-shrink-0 mb-2">
   <a href={web}
-     class="border rounded-lg text-xs sm:px-4 px-3 sm:py-3 py-2 border-opacity-30 mr-2 hover:border-blue-300 transition flex items-center"
+     class="border rounded-lg text-xs sm:px-4 px-3 sm:py-3 py-2 border-opacity-30 mr-2 hover:ring-2 {ringHoverClass} transition flex items-center group"
      target="_blank">
-    <Icon --icon-src="url('{icon}')" class="mr-3 w-[16px] h-[16px]" />
+    <Icon --icon-src="url('{icon}')" class="mr-3 w-[16px] h-[16px] {iconGroupHoverClass} transition" />
     {text}
   </a>
 </li>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,16 +24,63 @@
   <h2 class="font-bold text-xl mb-4 sm:mb-6">Techs I work with</h2>
 
   <ul class="flex flex-wrap max-w-2xl">
-    <TechPills text="Next" web="https://nextjs.org" icon="/icons/nextdotjs.svg" />
-    <TechPills text="Gatsby" web="https://gatsbyjs.com" icon="/icons/gatsby.svg" />
-    <TechPills text="React / Preact" web="https://reactjs.org" icon="/icons/react.svg" />
-    <TechPills text="Typescript" web="https://typescriptlang.org" icon="/icons/typescript.svg" />
-    <TechPills text="Tailwind" web="https://tailwindcss.com" icon="/icons/tailwindcss.svg" />
-    <TechPills text="Go" web="https://golang.org" icon="/icons/go.svg" />
-    <TechPills text="Prisma" web="https://www.prisma.io/" icon="/icons/prisma.svg" />
-    <TechPills text="Postgres" web="https://www.postgresql.org/" icon="/icons/postgresql.svg" />
-    <TechPills text="Kotlin" web="https://kotlinlang.org/" icon="/icons/kotlin.svg" />
-    <TechPills text="Android" web="https://developer.android.com/" icon="/icons/android.svg" />
+    <TechPills
+      text="Next" web="https://nextjs.org"
+      icon="/icons/nextdotjs.svg"
+      ringHoverClass="hover:ring-white"
+      iconGroupHoverClass="group-hover:bg-white" />
+    <TechPills
+      text="Gatsby" web="https://gatsbyjs.com"
+      icon="/icons/gatsby.svg"
+      ringHoverClass="hover:ring-[#663399]"
+      iconGroupHoverClass="group-hover:bg-white" />
+    <TechPills
+      text="React / Preact"
+      web="https://reactjs.org"
+      icon="/icons/react.svg"
+      ringHoverClass="hover:ring-[#61DAFB]"
+      iconGroupHoverClass="group-hover:bg-[#61DAFB]" />
+    <TechPills
+      text="Typescript"
+      web="https://typescriptlang.org"
+      icon="/icons/typescript.svg"
+      ringHoverClass="hover:ring-[#3178C6]"
+      iconGroupHoverClass="group-hover:bg-[#3178C6]" />
+    <TechPills
+      text="Tailwind" web="https://tailwindcss.com"
+      icon="/icons/tailwindcss.svg"
+      ringHoverClass="hover:ring-[#38B2AC]"
+      iconGroupHoverClass="group-hover:bg-[#38B2AC]" />
+    <TechPills
+      text="Go"
+      web="https://golang.org"
+      icon="/icons/go.svg"
+      ringHoverClass="hover:ring-[#00ADD8]"
+      iconGroupHoverClass="group-hover:bg-[#00ADD8]" />
+    <TechPills
+      text="Prisma"
+      web="https://www.prisma.io/"
+      icon="/icons/prisma.svg"
+      ringHoverClass="hover:ring-white"
+      iconGroupHoverClass="group-hover:bg-white" />
+    <TechPills
+      text="Postgres"
+      web="https://www.postgresql.org/"
+      icon="/icons/postgresql.svg"
+      ringHoverClass="hover:ring-[#4169E1]"
+      iconGroupHoverClass="group-hover:bg-white]" />
+    <TechPills
+      text="Kotlin"
+      web="https://kotlinlang.org/"
+      icon="/icons/kotlin.svg"
+      ringHoverClass="hover:ring-[#0095D5]"
+      iconGroupHoverClass="group-hover:bg-[#0095D5]" />
+    <TechPills
+      text="Android"
+      web="https://developer.android.com/"
+      icon="/icons/android.svg"
+      ringHoverClass="hover:ring-[#3DDC84]"
+      iconGroupHoverClass="group-hover:bg-[#3DDC84]" />
   </ul>
 </section>
 
