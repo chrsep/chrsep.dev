e2b83aa2bddc112eaf35f9d8dba5a0d3b95c3797
chris <chrsep@protonmail.com>
2021-12-23 13:00:45 +0700

    add mdsvex support.

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,5 @@
+import { mdsvex } from "mdsvex"
+import mdsvexConfig from "./mdsvex.config.js"
 import preprocess from "svelte-preprocess"
 import vercel from "@sveltejs/adapter-vercel"
 import windiCSS from "vite-plugin-windicss"
@@ -7,9 +9,11 @@ import svg from "@poppanator/sveltekit-svg"
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
+  extensions: [".svelte", ...mdsvexConfig.extensions],
+
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: [preprocess()],
+  preprocess: [preprocess(), mdsvex(mdsvexConfig)],
 
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
