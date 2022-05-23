4d430bed3fd8f46bf1e641ed2932f017f57de189
Chrisando E. Pramudhita <chrsep@protonmail.com>
2022-05-23 16:25:08 +0700

    add subtitle field.

--- apps/studio/schemas/post.js
+++ apps/studio/schemas/post.js
@@ -9,6 +9,11 @@ export default {
       type: "string",
     },
     {
+      name: "subtitle",
+      title: "Subtitle",
+      type: "string",
+    },
+    {
       name: "slug",
       title: "Slug",
       type: "slug",
